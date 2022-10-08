c0d16e0534a087a07e88669d977a5cadc08073bc
Михаил Картавцев <mkartavcev.corp@gmail.com>
2022-10-08 16:57:07 +0700

    Добавлены urls и view-функции

---
+++ yatube/posts/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+# Create your views here.
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+def group_posts(request, slug):
+    return HttpResponse('Посты')
