9dc83b7e2d387da57ff61309a00ef8ccd8a890eb
Misha Kartavcev <46868875+Hottys@users.noreply.github.com>
2023-03-26 16:54:55 +0700

    Продолжение проекта соц.сети

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from . import views
+from posts import views
+
+app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts)
-] 
+    path('', views.index, name='index'),
+    path('create/', views.post_create, name='post_create'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
+    path('profile/<str:username>/', views.profile, name='profile'),
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('posts/<post_id>/edit/', views.post_edit, name='edit'),
+]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,97 @@
-from django.http import HttpResponse
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+from .forms import PostForm
+from .models import Group, Post, User
 
-# Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+
+def index(request):
+    """Выводим на страницу первые 10 записей постов."""
+    post_list = Post.objects.select_related('author')
+    paginator = Paginator(post_list, settings.NUM_OF_POST)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
 
 def group_posts(request, slug):
-    return HttpResponse('Посты')
+    """Выводим на страницу первые 10 записей групп."""
+    group = get_object_or_404(Group, slug=slug)
+    posts = posts = group.posts.select_related('author')
+    paginator = Paginator(posts, settings.NUM_OF_POST)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    """Создаем паджинатор с 10 постами на одну страницу."""
+    author = get_object_or_404(User, username=username)
+    post_list = Post.objects.filter(author=author)
+    paginator = Paginator(post_list, settings.NUM_OF_POST)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+        'author': author
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    """Выводим на страницу подробную информацию о посте"""
+    post = get_object_or_404(Post, id=post_id)
+    posts_count = Post.objects.filter(author=post.author).count()
+    context = {
+        'post': post,
+        'posts_count': posts_count,
+        'requser': request.user
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    """Создаем форму для создания поста."""
+    form = PostForm(request.POST)
+    if form.is_valid():
+        post_create = form.save(commit=False)
+        post_create.author = request.user
+        post_create.save()
+        return redirect(
+            'posts:profile', post_create.author
+        )
+    context = {
+        'form': form
+    }
+    return render(request, 'posts/post_create.html', context)
+
+
+@login_required
+def post_edit(request, post_id):
+    """Создаем форму для редактирования поста."""
+    edit_post = get_object_or_404(Post, id=post_id)
+    if request.user != edit_post.author:
+        return redirect(
+            'posts:post_detail', post_id
+        )
+    form = PostForm(request.POST or None, instance=edit_post)
+    if form.is_valid():
+        form.save()
+        return redirect(
+            'posts:post_detail', post_id
+        )
+    context = {
+        'form': form,
+        'is_edit': True
+    }
+    return render(request, 'posts/post_create.html', context)
